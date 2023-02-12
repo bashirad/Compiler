858f0409e9c05d4bd1c3f3b3fe7010438e30b76b
bashirad <79177010+bashirad@users.noreply.github.com>
2023-02-12 15:29:57 -0500

    Update Lexer.java

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -8,6 +8,21 @@ public class Lexer {
 
     private static String path;
 
+    private static String OPEN_BLOCK;
+    private static String CLOSE_BLOCK;
+    private static String ID;
+    private static String I_TYPE;
+    private static String EOP;
+    private static String ASSIGN_OP;
+    private static String OPEN_BRACKET;
+    private static String CLOSE_BRACKET;
+    private static String PRINT_OP;
+    private static String OPEN_QUOTES;
+    private static String CLOSE_QUOTES;
+    private static String INT;
+    private static String WHITE_SPACE;
+
+
     public static void init() {
 
         /** Get the absolute path of the source code */
@@ -15,17 +30,6 @@ public class Lexer {
         //Scanner scan = new Scanner(System.in);
         //path = scan.nextLine();
 
-
-        /** Read the file data */
-        Lexer.readFile("C:\\Users\\Bashir\\Documents\\Bashirs_Code_all\\Java\\cmpt432\\src\\code.txt");
-        Tokens token1;
-        if (1 == 1) {
-            token1 = new Tokens("OPEN BLOCK", "{", 1, 1);
-        }
-
-        /** Log information out to the output screen */
-        log(1, true, "Lexer", token1.lexemeName, token1.symbol,
-                token1.lineNum, token1.positionNum);
     }
 
     /** Read the source code that is the file */
@@ -35,7 +39,7 @@ public class Lexer {
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                /** System.out.println(data);   */
+                System.out.println(data);
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -48,8 +52,7 @@ public class Lexer {
     public static void log(int progNum, Boolean debug, String compilerStage, String tokenName, String tokenSymbol,
                     int tokenLineNum, int tokenPosNum) {
         if (debug) {
-            System.out.println("INFO Lexer - Lexing program " + progNum + " ... ");
-            System.out.println("DEBUG Lexer - OPEN_BLOCK [ { ] found at (1:1)");
+            //if (progNum == 1) System.out.println("INFO Lexer - Lexing program " + progNum + " ... ");
             System.out.println("DEBUG " + compilerStage + " - " + tokenName + " [ " + tokenSymbol + " ] found at ("
                                 + tokenLineNum + ":" + tokenPosNum + ")");
         }
