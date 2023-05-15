655b74adb88d5569f9b6f396e767380fb71d582d
Bashir Dahir <79177010+bashirad@users.noreply.github.com>
2023-05-15 16:32:32 -0400

    almost done

--- src/Jumps.java
+++ src/Jumps.java
@@ -13,8 +13,27 @@ public class Jumps {
     }
 
     public int getJumpDistance(String jumpName) {
+        if (jumpTable.get(jumpName) < 10 ) {
+            return jumpTable.get(jumpName);
+        }
         return jumpTable.get(jumpName);
     }
+
+    public void setJumpDistance(String jumpName, int distance) {
+        jumpTable.remove(jumpName);
+         jumpTable.put(jumpName, distance);
+    }
+
+    public void printTable() {
+        System.out.println("+------------------+----------+");
+        System.out.println("| Jump Name        | Distance |");
+        System.out.println("+------------------+----------+");
+        for (Map.Entry<String, Integer> entry : jumpTable.entrySet()) {
+            System.out.printf("| %-16s | %8d |%n", entry.getKey(), entry.getValue());
+        }
+        System.out.println("+------------------+----------+");
+    }
+
 
     public boolean hasJump(String jameName) {
         return jumpTable.containsKey(jameName);
